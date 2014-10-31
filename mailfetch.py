4498076ec4925a3aa826819c962102b9197bc939
Chad Klusek <klusek@uky.edu>
2014-10-31 03:03:15 -0400

    Fix mailfetch to only save .stl and .obj files

--- mailfetch.py
+++ mailfetch.py
@@ -1,6 +1,6 @@
-import imaplib	  # Facilitates connection to mailserver
-import email	  # Facilitates message parsing/extraction
-import os	      # Used to construct attachment save path
+import imaplib    # Facilitates connection to mailserver
+import email      # Facilitates message parsing/extraction
+import os         # Used to construct attachment save path
 import config
 import pipeline
 import getpass
@@ -21,62 +21,69 @@ import logger
 mail_password = None
 mlogger = logger.Logger('mailfetch')
 def initialize():
-	global mail_password
-	if mail_password == None:
-		mail_password = get_password()
-	
+    global mail_password
+    if mail_password == None:
+        mail_password = get_password()
+    
 # reads email, gets attachments, returns all attachments as list
 def poll(verbose = True):
-	global mail_password
-	if mail_password == None:
-		mail_password = get_password()
+    global mail_password
+    if mail_password == None:
+        mail_password = get_password()
 
-	mailfetch_config = config.read_config()
+    mailfetch_config = config.read_config()
 
-	servername = mailfetch_config["Mailfetch"]["server"]
-	portnumber = mailfetch_config["Mailfetch"]["port"]
-	username = mailfetch_config["Mailfetch"]["username"]
-	mailbox = mailfetch_config["Mailfetch"]["mailbox"]
-	savedir = mailfetch_config["Mailfetch"]["savedir"]
+    servername = mailfetch_config["Mailfetch"]["server"]
+    portnumber = mailfetch_config["Mailfetch"]["port"]
+    username = mailfetch_config["Mailfetch"]["username"]
+    mailbox = mailfetch_config["Mailfetch"]["mailbox"]
+    savedir = mailfetch_config["Mailfetch"]["savedir"]
+    allowedtypes = mailfetch_config["Mailfetch"]["extensions"]
 
-	connection = open_connection(servername,portnumber)
+    connection = open_connection(servername,portnumber)
 
-	login(connection,username,mail_password,mailbox)
+    login(connection,username,mail_password,mailbox)
 
-	infolist = []
-	messagelist = get_message_list(connection)
-	for message in messagelist:
-		email = get_message_contents(connection,message)
+    infolist = []
+    messagelist = get_message_list(connection)
+    for message in messagelist:
+        email = get_message_contents(connection,message)
 
-		# If the email is not multipart, don't process further.
-		# Only multipart messages have attachments.
-		if email.get_content_maintype() != 'multipart':
-			continue
+        # If the email is not multipart, don't process further.
+        # Only multipart messages have attachments.
+        if email.get_content_maintype() != 'multipart':
+            continue
 
-		# Iterate over the parts of an individual message
-		for part in email.walk():
-			# An attachment itself is NOT multipart
-			if part.get_content_maintype() == 'multipart':
-				continue
+        # Iterate over the parts of an individual message
+        for part in email.walk():
+            # An attachment itself is NOT multipart
+            if part.get_content_maintype() == 'multipart':
+                continue
 
-			# A condition where no attachment is to be found
-			if part.get('Content-Disposition') is None:
-				continue
+            # A condition where no attachment is to be found
+            if part.get('Content-Disposition') is None:
+                continue
 
-			# If we make it this far, extract and save the attachment
-			path = extract_attachment(part,savedir)
-			
-			# put all the information here
-			sendaddr = get_sender_addr(connection,message)
-			jobinfo = pipeline.PrintJob(path,sendaddr)
-			infolist.append(jobinfo)
+            # Only proceed if the attachment extension matches those allowed
+            filename = part.get_filename()
+            extension = filename[-4:-1]
+            if extension not in allowedtypes:
+                continue
 
-	#write_config(mailfetch_config,mailfetch_config_filename)
+            # If we make it this far, extract and save the attachment
+            path = extract_attachment(part,savedir)
+            
+            # put all the information here
+            sendaddr = get_sender_addr(connection,message)
+            jobinfo = pipeline.PrintJob(path,sendaddr)
+            infolist.append(jobinfo)
 
-	# Clean up when done
-	connection.close()
-	connection.logout()
-	return infolist
+    #write_config(mailfetch_config,mailfetch_config_filename)
+
+    # Clean up when done
+    connection.close()
+    connection.logout()
+    return infolist
 
 # Prompt user to enter password for the default email account
 # ~AW: This will have to be automated eventually
@@ -120,33 +127,33 @@ def get_message_contents(socket,msg):
     return contents
 
 def get_sender_addr(socket,msg):
-	# There is some redundancy here, as this function initially does everytning
-	# in the first 3 lines of get_message_contents(). Improve Later !!!
-	typ, data = socket.fetch(msg, "(RFC822)")
-	body = data[0][1]
-	contents = email.message_from_bytes(body)
-	sender = email.utils.parseaddr(contents['From'])
-	mlogger.log(sender[1])
-	return sender[1] # The sender addr
-	
+    # There is some redundancy here, as this function initially does everytning
+    # in the first 3 lines of get_message_contents(). Improve Later !!!
+    typ, data = socket.fetch(msg, "(RFC822)")
+    body = data[0][1]
+    contents = email.message_from_bytes(body)
+    sender = email.utils.parseaddr(contents['From'])
+    mlogger.log(sender[1])
+    return sender[1] # The sender addr
+    
 def extract_attachment(attachment,path):
-	filename = attachment.get_filename()
-	counter = 0
+    filename = attachment.get_filename()
+    counter = 0
 
     # If there is no filename... create one (Think about this !!!)
     # Also, join the filename and directory path to construct final save location
-	if not filename:
-		filename = 'attachment-%03d%s' % (counter, '.sav')
-		counter += 1
-	save_path = os.path.join(path,filename)
+    if not filename:
+        filename = 'attachment-%03d%s' % (counter, '.sav')
+        counter += 1
+    save_path = os.path.join(path,filename)
 
     # Don't overwrite a preexisting file with the same name
-	if not os.path.isfile(save_path):
-		save_attachment(save_path,attachment)
-	else:
-		# should this throw an exception?
-		mlogger.log(save_path,"already exists ...")
-	return save_path
+    if not os.path.isfile(save_path):
+        save_attachment(save_path,attachment)
+    else:
+        # should this throw an exception?
+        mlogger.log(save_path,"already exists ...")
+    return save_path
 
 def save_attachment(location,attachment):
     fileptr = open(location,'wb')
@@ -157,5 +164,5 @@ def save_attachment(location,attachment):
     return 0
 
 if __name__ == '__main__':
-	initialize()
-	poll()
+    initialize()
+    poll()
