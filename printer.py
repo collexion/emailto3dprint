3334cb951bd6cc78bb63890d17e7f2fd53bdd29f
Andrew Ward <andrew.ward@uky.edu>
2014-11-09 12:41:43 -0500

    Idunnno what these changes are.

--- printer.py
+++ printer.py
@@ -8,12 +8,15 @@ def send_job(jobinfo):
 	
 	#read  config file information
 	conf = config.read_config()
-	print_command = conf['Printer']['Path']
-	pronsole_commands = conf['Printer']['PrintCommandsPath']
+	encode_command = conf['Printer']['Encoder']
 	
 	# determine input and output filenames
 	infile = jobinfo.previous_file()
+	root, ext = os.path.splitext(infile)
+	encoded_file = '/dev/tty/usb0/' + os.path.basename(root) + ".x3g"
 	
+	# convert to .x3g
+	encode_command = '{0} {1} {2}'.format(encode_command, 
 	# run command
 	command = '{0} {1} < {2}'.format(print_command, infile, pronsole_commands)
 		
